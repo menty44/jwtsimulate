7fa72126c0217fc67b74d98452e69994fe577379
Fredrick Oluoch <menty44@gmail.com>
2023-11-08 15:38:35 +0300

    JWT using pub/priv keys

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express();
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
-
-
+const crypto = require("crypto");
 
 // Load private and public keys
 const privateKey = fs.readFileSync('private-key.pem');
@@ -15,10 +14,44 @@ const payload = {
   roles: ['user', 'admin'],
 };
 
-// Sign the JWT with the private key
-const token = jwt.sign(payload, privateKey, { algorithm: 'RS256' });
+const encryptedData = crypto.publicEncrypt(
+  {
+    key: publicKey,
+    padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+    oaepHash: "sha256",
+  },
+  // We convert the data string to a buffer using `Buffer.from`
+  Buffer.from(JSON.stringify(payload))
+);
 
-console.log('Token:', token);
+// console.log('checker ', encryptedData);
+console.log('checker ', encryptedData.toString("base64"));
+
+// Sign the JWT with the private key
+// const token = jwt.sign(payload, privateKey, { algorithm: 'RS256' });
+const token = jwt.sign({payload: encryptedData.toString("base64")}, privateKey, { algorithm: 'RS256' });
+console.log('\n\n')
+console.log('Token: ', token);
+
+const jwtDecode = jwt.decode(token, true);
+console.log('\n\n')
+console.log('decode from jwt payload', jwtDecode);
+console.log('decode ', jwtDecode.payload);
+console.log('\n\n')
+
+const decryptedData = crypto.privateDecrypt(
+  {
+    key: privateKey,
+    // In order to decrypt the data, we need to specify the
+    // same hashing function and padding scheme that we used to
+    // encrypt the data in the previous step
+    padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+    oaepHash: "sha256",
+  },
+  Buffer.from(jwtDecode.payload, "base64")
+);
+
+console.log('decoded payload 1', decryptedData.toString());
 
 // app.get('/', function (req, res) {
 // res.json({name: "Welocme to GeeksforGeeks!"});
